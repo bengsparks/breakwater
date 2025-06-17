ecdd565b2dffe13fc10a52cff13619f54841bc07
Benjamin Sparks <b.sparks@alugha.com>
2025-06-17 17:53:59 +0200

    feat(socket2): switch to VPP network stack

--- default.nix
+++ default.nix
@@ -4,35 +4,50 @@
   ...
 }:
 let
+  inherit (pkgs) rust-toolchain;
+
+  overrides = {
+    vncserver = attrs: {
+      nativeBuildInputs =
+        (attrs.nativeBuildInputs or [ ])
+        ++ (with pkgs; [
+          pkg-config
+          clang
+        ]);
+
+      buildInputs =
+        (attrs.buildInputs or [ ])
+        ++ (with pkgs; [
+          libvncserver
+        ]);
+
+      env = {
+        LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+        LIBVNCSERVER_HEADER_FILE = "${pkgs.libvncserver.dev}/include/rfb/rfb.h";
+      };
+    };
+
+    # This affects the vendored version of `socket2`, called `breakwater-socket2`, which links against
+    # `-lvcl_ldpreload` from the `pkgs.vpp` package.
+    socket2 = attrs: {
+      buildInputs =
+        (attrs.buildInputs or [ ])
+        ++ (with pkgs; [
+          vpp
+        ]);
+
+      env.VPP_LIBS = "${pkgs.vpp}/lib";
+    };
+  };
+
   cargoNix = import cargoDotNix {
     inherit pkgs;
     buildRustCrateForPkgs =
       pkgs:
       pkgs.buildRustCrate.override {
-        defaultCrateOverrides = pkgs.defaultCrateOverrides // {
-          vncserver = attrs: {
-            nativeBuildInputs =
-              (attrs.nativeBuildInputs or [ ])
-              ++ (with pkgs; [
-                pkg-config
-                clang
-              ]);
-
-            buildInputs =
-              (attrs.buildInputs or [ ])
-              ++ (with pkgs; [
-                libvncserver
-                libvncserver.dev
-              ]);
-
-            env = {
-              LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
-              LIBVNCSERVER_HEADER_FILE = "${pkgs.libvncserver.dev}/include/rfb/rfb.h";
-            };
-          };
-        };
-        rustc = pkgs.rust-toolchain;
-        cargo = pkgs.rust-toolchain;
+        defaultCrateOverrides = pkgs.defaultCrateOverrides // overrides;
+        rustc = rust-toolchain;
+        cargo = rust-toolchain;
       };
   };
 
@@ -43,4 +58,5 @@ in
 {
   breakwater-egui = breakwater.override { features = [ "egui" ]; };
   breakwater-vnc = breakwater.override { features = [ "vnc" ]; };
+  breakwater-socket2 = workspace.socket2.build;
 }
